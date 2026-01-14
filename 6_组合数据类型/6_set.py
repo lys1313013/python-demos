ea5fb6c8c822896bfa9d690644314c65fd5d084f
lys1313013 <lys1313013@gmail.com>
2026-01-14 22:47:07 +0800

    更新 set

---
+++ 6_组合数据类型/6_set.py
@@ -0,0 +1,22 @@
+s = set()
+print("打印类型")
+print(s, type(s))
+
+s = {1,2,3,4}
+print(s, type(s))
+
+print("字符串转集合")
+s = set('abc')
+print(s)
+
+print("往集合中添加多个元素")
+s.update({'c', 'd', 'f'})
+print(s)
+
+
+print("取交集")
+s2 = {'d', 'c'}
+print(s & s2)
+
+print("取并集")
+print(s | s2)
